a3bbd93a6c64774726e5da681b78a93793257a10
Gabriel Titeux <72764917+Xelone28@users.noreply.github.com>
2023-05-30 13:56:10 +0200

    First commit

---
+++ src/test/java/org/example/CheckHTTPResponse.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldPassIfStringMatches() throws Exception {
+        assert (restTemplate.getForObject("http://localhost:8081/users/test",
+                String.class)).contains("Hello World");
+    }
+}
